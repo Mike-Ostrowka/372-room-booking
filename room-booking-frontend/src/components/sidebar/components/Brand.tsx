2b518accac6ac8bd8b04bb8cccf43886e955ca7e
chhokara <chhokara@gmail.com>
2023-03-03 12:44:14 -0800

    add sfu logo and cleanup dashboard

--- room-booking-frontend/src/components/sidebar/components/Brand.tsx
+++ room-booking-frontend/src/components/sidebar/components/Brand.tsx
@@ -1,20 +1,22 @@
+// Image
+import sfssLogo from "assets/img/logos/sfss-logo.png";
 // Chakra imports
-import { Flex, useColorModeValue } from '@chakra-ui/react';
+import { Flex, useColorModeValue, Image } from "@chakra-ui/react";
 
 // Custom components
-import { HorizonLogo } from 'components/icons/Icons';
-import { HSeparator } from 'components/separator/Separator';
+import { HorizonLogo } from "components/icons/Icons";
+import { HSeparator } from "components/separator/Separator";
 
 export function SidebarBrand() {
-	//   Chakra color mode
-	let logoColor = useColorModeValue('navy.700', 'white');
+  //   Chakra color mode
+  let logoColor = useColorModeValue("navy.700", "white");
 
-	return (
-		<Flex alignItems='center' flexDirection='column'>
-			<HorizonLogo h='26px' w='175px' my='32px' color={logoColor} />
-			<HSeparator mb='20px' />
-		</Flex>
-	);
+  return (
+    <Flex alignItems="center" flexDirection="column">
+      <Image width="50%" marginBottom={5} src={sfssLogo} />
+      <HSeparator mb="20px" />
+    </Flex>
+  );
 }
 
 export default SidebarBrand;
