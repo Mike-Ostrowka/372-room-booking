8ff275b964a2dfa56e9d325f8434e9ca530b9a7e
chhokara <chhokara@gmail.com>
2023-03-09 11:43:56 -0800

    setup user context

--- room-booking-frontend/src/index.tsx
+++ room-booking-frontend/src/index.tsx
@@ -7,21 +7,24 @@ import AuthSignInLayout from "./layouts/auth/AuthSignIn";
 import AdminLayout from "./layouts/admin";
 import RTLLayout from "./layouts/rtl";
 import { ChakraProvider } from "@chakra-ui/react";
+import { UserProvider } from "contexts/UserContext";
 import theme from "./theme/theme";
 
 ReactDOM.render(
   <ChakraProvider theme={theme}>
-    <React.StrictMode>
-      <HashRouter>
-        <Switch>
-          <Route path={`/auth/sign-up`} component={AuthSignUpLayout} />
-          <Route path={`/auth/sign-in`} component={AuthSignInLayout} />
-          <Route path={`/admin`} component={AdminLayout} />
-          <Route path={`/rtl`} component={RTLLayout} />
-          {/* <Redirect from="/" to="/admin" /> */}
-        </Switch>
-      </HashRouter>
-    </React.StrictMode>
+    <UserProvider>
+      <React.StrictMode>
+        <HashRouter>
+          <Switch>
+            <Route path={`/auth/sign-up`} component={AuthSignUpLayout} />
+            <Route path={`/auth/sign-in`} component={AuthSignInLayout} />
+            <Route path={`/admin`} component={AdminLayout} />
+            <Route path={`/rtl`} component={RTLLayout} />
+            {/* <Redirect from="/" to="/admin" /> */}
+          </Switch>
+        </HashRouter>
+      </React.StrictMode>
+    </UserProvider>
   </ChakraProvider>,
   document.getElementById("root")
 );
