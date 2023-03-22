48f151194e7930bec5a90ca99653ccf3b69181f1
chhokara <chhokara@gmail.com>
2023-03-21 17:50:19 -0700

    build skeleton for calendar and statistics

--- room-booking-frontend/src/views/admin/default/index.tsx
+++ room-booking-frontend/src/views/admin/default/index.tsx
@@ -1,10 +1,15 @@
 // Chakra imports
-import { Box } from "@chakra-ui/react";
+import { Box, Stack } from "@chakra-ui/react";
+import MiniCalendar from "components/calendar/MiniCalendar";
+import Statistics from "./components/Statistics";
 
 export default function Dashboard() {
   return (
     <Box pt={{ base: "130px", md: "80px", xl: "80px" }}>
-      <div>Dashboard</div>
+      <Stack width="100%" alignItems="center" spacing={2}>
+        <MiniCalendar selectRange={false} />
+        <Statistics />
+      </Stack>
     </Box>
   );
 }
