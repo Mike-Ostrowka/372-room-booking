68db47c9dab1e902a787bae6e3fffdd8860aa2f0
chhokara <chhokara@gmail.com>
2023-03-29 16:53:07 -0700

    transpile typescript

--- ryan_express/routes/middleware/isLoggedIn.js
+++ ryan_express/routes/middleware/isLoggedIn.js
@@ -1,5 +1,5 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+exports.__esModule = true;
 // Middleware to check if the user is logged in
 function isLoggedIn(request, response, next) {
     if (request.session.user) {
@@ -11,4 +11,4 @@ function isLoggedIn(request, response, next) {
         response.json({ success: false });
     }
 }
-exports.default = isLoggedIn;
+exports["default"] = isLoggedIn;
