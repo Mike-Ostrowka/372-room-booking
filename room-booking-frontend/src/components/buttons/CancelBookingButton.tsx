42bbb215bb2f7f4a29567def5db4da62011b63cf
mostrowk <mostrowk@sfu.ca>
2023-04-03 21:44:31 -0800

    add confirmation that user wants to delete a booking

--- room-booking-frontend/src/components/buttons/CancelBookingButton.tsx
+++ room-booking-frontend/src/components/buttons/CancelBookingButton.tsx
@@ -1,11 +1,14 @@
-import { Button, Stack, Toast, useToast } from "@chakra-ui/react";
+import { AlertDialog, AlertDialogBody, AlertDialogContent, AlertDialogFooter, AlertDialogHeader, AlertDialogOverlay, Button, Stack, Toast, useDisclosure, useToast } from "@chakra-ui/react";
 import { UserContext } from "contexts/UserContext";
-import { useContext } from "react";
+import { useContext, useRef } from "react";
 
 const CancelBookingButton = (props: {booking_id:any, isHidden: any, onClose: any} ) => {
 
       const { loggedInUser } = useContext(UserContext);
       const toast = useToast();
+
+      const { isOpen, onOpen, onClose } = useDisclosure();
+      const cancelRef = useRef();
 
     const handleClick = async () => {
         const response = await fetch(`http://localhost:8080/room-booking/`, {
@@ -29,19 +32,44 @@ const CancelBookingButton = (props: {booking_id:any, isHidden: any, onClose: any
               isClosable: true,
             });
         }
+        onClose();
         props.onClose();
     }
 
-    return props.isHidden? (
-        <Stack>
-            <Button onClick={handleClick} 
-            colorScheme="red"
-            mt="20px"
-            >
-                Cancel Booking
-            </Button>
-        </Stack>
-    ): null
+    return props.isHidden ? (
+      <Stack>
+        <Button onClick={onOpen} colorScheme="red" mt="20px">
+          Cancel Booking
+        </Button>
+
+        <AlertDialog
+          isOpen={isOpen}
+          leastDestructiveRef={cancelRef}
+          onClose={onClose}
+        >
+          <AlertDialogOverlay>
+            <AlertDialogContent>
+              <AlertDialogHeader fontSize="lg" fontWeight="bold">
+                Delete Booking
+              </AlertDialogHeader>
+
+              <AlertDialogBody>
+                Are you sure? You can't undo this action afterwards.
+              </AlertDialogBody>
+
+              <AlertDialogFooter>
+                <Button ref={cancelRef} onClick={onClose}>
+                  Cancel
+                </Button>
+                <Button colorScheme="red" onClick={handleClick} ml={3}>
+                  Delete
+                </Button>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialogOverlay>
+        </AlertDialog>
+      </Stack>
+    ) : null;
 
 }
 
