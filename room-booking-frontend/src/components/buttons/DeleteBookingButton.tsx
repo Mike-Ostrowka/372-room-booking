37dbba2c0579d2ab0c351cdac7528509def24afb
mostrowk <mostrowk@sfu.ca>
2023-04-03 21:31:21 -0800

    add delete booking button for bookings in the future

---
+++ room-booking-frontend/src/components/buttons/DeleteBookingButton.tsx
@@ -0,0 +1,48 @@
+import { Button, Stack, Toast, useToast } from "@chakra-ui/react";
+import { UserContext } from "contexts/UserContext";
+import { useContext } from "react";
+
+const DeleteBookingButton = (props: {booking_id:any, isHidden: any, onClose: any} ) => {
+
+      const { loggedInUser } = useContext(UserContext);
+      const toast = useToast();
+
+    const handleClick = async () => {
+        const response = await fetch(`http://localhost:8080/room-booking/`, {
+          method: "DELETE",
+          credentials: "include",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            booking_id: props.booking_id,
+            user_id: loggedInUser.u_id,
+          }),
+        });
+        const responseData = await response.json();
+        if(responseData.length == 0) {
+            toast({
+              title: "Deleted Booking",
+              description: "Booking deleted successfully",
+              status: "success",
+              duration: 5000,
+              isClosable: true,
+            });
+        }
+        props.onClose();
+    }
+
+    return props.isHidden? (
+        <Stack>
+            <Button onClick={handleClick} 
+            colorScheme="red"
+            mt="20px"
+            >
+                Delete Booking
+            </Button>
+        </Stack>
+    ): null
+
+}
+
+export default DeleteBookingButton;
