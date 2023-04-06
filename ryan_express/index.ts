143185cfd7cf110624fbacf2549ed4433b8488ee
chhokara <chhokara@gmail.com>
2023-04-05 20:49:01 -0700

    increase session age

--- ryan_express/index.ts
+++ ryan_express/index.ts
@@ -38,7 +38,7 @@ app.use(
     name: "session",
     secret: "testsecretpleasechange",
     resave: false,
-    cookie: { maxAge: 60 * 60 * 1000 },
+    cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 },
     saveUninitialized: true,
   })
 );
